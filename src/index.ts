8ec9a8246e09c77a476d2f259b8ef2ceec155f3b
Jonatha Gomes <contato.jonathagomesdev@gmail.com>
2024-10-11 18:51:06 -0300

    feat: add funnel in bot

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,8 @@ type Users = {
 };
 const users: { [chatId: string]: Users } = {};
 
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const welcomeMessage = `
 Bem-vindo ao Grupo de Sinais! Aqui você encontrará os melhores sinais, com 97% de assertividade ✅
 
@@ -28,12 +30,131 @@ const selectMessage = `
 👇👇👇👇👇👇
 `;
 
-bot.onText(/\/start/, (msg) => {
+const minesSelectedMessage =
+  "Você selecionou: 💎 Mines. Para continuar faça o cadastro na plataforma clicando no botão abaixo!";
+
+bot.onText(/\/start/, async (msg) => {
   const chatId = msg.chat.id;
   users[chatId] = { chatId: chatId, whatsapp: "" };
-  // bot.sendMessage(chatId, "mensagem", {
-  //   reply_markup: {
-  //     inline_keyboard: [text: "", url]
-  //   }
-  // });
+
+  await bot.sendChatAction(chatId, "typing");
+  await delay(2000);
+
+  await bot.sendMessage(chatId, welcomeMessage);
+
+  await bot.sendChatAction(chatId, "typing");
+  await delay(2000);
+
+  await bot.sendMessage(chatId, selectMessage, {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "💎 Mines",
+            callback_data: "mines",
+          },
+        ],
+      ],
+    },
+  });
+});
+
+bot.on("callback_query", async (query) => {
+  const chatId = query.message?.chat.id || "";
+
+  if (query.data === "mines") {
+    await bot.sendChatAction(chatId, "typing");
+    await delay(2000);
+
+    await bot.sendMessage(chatId, minesSelectedMessage);
+
+    await bot.sendChatAction(chatId, "typing");
+    await delay(1000);
+
+    await bot.sendMessage(chatId, "Clique no botão abaixo para se cadastrar:", {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Cadastro",
+              url: affiliateLink,
+            },
+          ],
+        ],
+      },
+    });
+
+    await bot.sendChatAction(chatId, "typing");
+    await delay(5000);
+
+    await bot.sendMessage(
+      chatId,
+      "Você se cadastrou? Responda apenas SIM ou NÃO."
+    );
+  }
+});
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+
+  if (msg.text?.toLowerCase() == "sim") {
+    await bot.sendChatAction(chatId, "typing");
+    await delay(2000);
+
+    await bot
+      .sendMessage(
+        chatId,
+        "Por favor, envie o seu número do WhatsApp para entrega de prêmios:"
+      )
+      .then(() => {
+        bot.once("message", async (msg) => {
+          const whatsappNumber = msg.text || "";
+          users[chatId].whatsapp = whatsappNumber;
+
+          await bot.sendChatAction(chatId, "typing");
+          await delay(2000);
+
+          await bot.sendMessage(
+            chatId,
+            "Obrigado! Você agora tem acesso. Aqui está o botão de entrar:"
+          );
+
+          await bot.sendChatAction(chatId, "typing");
+          await delay(2000);
+
+          await bot.sendMessage(
+            chatId,
+            "Clique no botão para entrar no grupo:",
+            {
+              reply_markup: {
+                inline_keyboard: [
+                  [
+                    {
+                      text: "ACESSAR GRUPO",
+                      url: channelLink,
+                    },
+                  ],
+                ],
+              },
+            }
+          );
+
+          await delay(2000);
+          await bot.sendMessage(
+            channelSaveId,
+            `NOVO USUÁRIO: \nChat ID: ${chatId} \nWhatsapp: ${whatsappNumber}`
+          );
+        });
+      });
+  } else if (
+    msg.text?.toLowerCase() == "nao" ||
+    msg.text?.toLowerCase() == "não"
+  ) {
+    await bot.sendChatAction(chatId, "typing");
+    await delay(2000);
+    await bot.sendMessage(
+      chatId,
+      "Por favor, faça seu cadastro e digite 'sim' quando terminar ou 'não' para tentar novamente."
+    );
+  }
 });
